bb3babb9cdf3027a991a0fa5b55559830decad38
taikon <davidma100@gmail.com>
2024-05-27 22:17:58 -0600

    Update realtime collaboration.

--- medicalcharting/README.md
+++ medicalcharting/README.md
@@ -1,12 +1,13 @@
-# React Frontend
+# Medical Charting App - React Frontend
 
-### Getting Started
-- Development
-  ```
+### Introduction
+- This is the frontend portion of the medical-charting-app.
+
+### Quickstart
+- Add environment variables. `VITE_FASTAPI_SERVER_ACCESS_TOKEN` should be the same as `ACCESS_TOKEN` in the FastAPI server.
+- Install dependencies and run the development server
+  ```bash
   pnpm install
   pnpm run dev
   ```
 
-- Production
-  ```
-  ```
--- medicalcharting/src/App.tsx
+++ medicalcharting/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import {Textarea} from '@/components/ui/textarea'
 import {Button} from '@/components/ui/button'
 import axios from 'axios'
 
@@ -14,16 +13,22 @@ import Collaboration from '@tiptap/extension-collaboration'
 import { TiptapCollabProvider } from '@hocuspocus/provider'
 
 function App() {
-  const [content, setContent] = useState('')
-  const [key, setKey] = useState(0)
+  const [text, setText] = useState('')
 
   function handleClear() {
-    setKey(key + 1)
+    if (!editor) {
+      return
+    }
+    editor.commands.clearContent()
   }
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   async function handleCreateMessage() {
+    if (!editor) {
+      return
+    }
+
     setIsSubmitting(true); // Disable button when the POST request starts
 
     const createMessageUrl = import.meta.env.VITE_FASTAPI_SERVER_API_BASE_URL + '/api/messages'
@@ -37,13 +42,13 @@ function App() {
       const response = await axios.post(
         createMessageUrl,
         {
-          prompt: content
+          prompt: text
         },
         { headers }
       )
 
       if (response.data) {
-        setContent(response.data.prediction)
+        editor.commands.setContent(response.data.prediction)
       }
     } catch (error) {
       console.error(error)
@@ -56,13 +61,11 @@ function App() {
 
   // Connect to your Collaboration server
   const provider = new TiptapCollabProvider({
-    name: "document.name", // Unique document identifier for syncing. This is your document name.
-    // appId: import.meta.env.VITE_TIPTAP_COLLAB_APP_ID, 
-    appId: '7j9y6m10', // Cloud Dashboard AppID 
-    token: 'notoken', // JWT token
+    name: "document.name",
+    appId: import.meta.env.VITE_TIPTAP_COLLAB_APP_ID, 
+    token: import.meta.env.VITE_TIPTAP_JWT,
     document: doc,
   })
-
 
   const editor = useEditor({
     extensions: [
@@ -74,42 +77,42 @@ function App() {
       }),
     ],
     content: `
-      <p>
-        This is a radically reduced version of tiptap. It has support for a document, with paragraphs and text. That’s it. It’s probably too much for real minimalists though.
-      </p>
-      <p>
-        The paragraph extension is not really required, but you need at least one node. Sure, that node can be something different.
-      </p>
+      <p></p>
     `,
   })
 
-  return (
-    <>
-      <div className="hidden h-full flex-col md:flex">
-        <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
-          <h2 className="text-lg font-semibold">Application</h2>
-          <div className="ml-auto flex w-full space-x-2 sm:justify-end">
-            <Button onClick={handleClear}>Clear</Button>
-            <Button onClick={handleCreateMessage} disabled={isSubmitting}>
-              {isSubmitting ? "Creating..." : "Create Message"}
-            </Button>
-          </div>
+  function handleClick() {
+    if (!editor) {
+      return
+    }
 
-        </div>
-        <div className="flex-1">
-          <div className="container h-full py-6">
-            {/* <Textarea */}
-            {/*   key={key} */}
-            {/*   defaultValue={content} */}
-            {/**/}
-            {/*   className="min-h-[400px] flex-1 p-4 md:min-h-[700px] lg:min-h-[700px]" */}
-            {/* /> */}
+    editor.commands.setContent('Hello World')
+  }
+
+  if (editor) {
+    return (
+      <>
+        <div className="hidden h-full flex-col md:flex">
+          <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
+            <h2 className="text-lg font-semibold">Application</h2>
+            <div className="ml-auto flex w-full space-x-2 sm:justify-end">
+              <Button onClick={handleClear}>Clear</Button>
+              <Button onClick={handleCreateMessage} disabled={isSubmitting}>
+                {isSubmitting ? "Creating..." : "Create Message"}
+              </Button>
+              <Button onClick={handleClick}>Set Content</Button>
+            </div>
+
+          </div>
+          <div className="flex-1">
+            <div className="container h-full py-6">
+              <EditorContent editor={editor} className='ring-1 ring-gray-500 rounded-lg lg:h-[500px] md:h-[500px] sm:h-[300px] h-[300px]' />
+            </div>
           </div>
         </div>
-      </div>
-    <EditorContent editor={editor} className='ring-1 ring-gray-500' />
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default App
